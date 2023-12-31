831a310c6f890dbfdddb99a685d4200d86b41ffd
sainathhiwale <sainathhiwale2@gmail.com>
2023-12-31 23:54:18 +0530

    main is created for pull

--- app/src/main/java/com/examen/room_database/data/database/AppDatabase.kt
+++ app/src/main/java/com/examen/room_database/data/database/AppDatabase.kt
@@ -5,7 +5,7 @@ import androidx.room.RoomDatabase
 import com.examen.room_database.data.UserRegister
 import com.examen.room_database.data.entity.dao.RoomDao
 
-
+//main branch is created for take pull of coroutine-flow mvvm branch
 @Database(entities = [UserRegister::class], version = 1 )
 abstract class AppDatabase:RoomDatabase() {
 
